d8a6adb053d3c0766a4b260459035b4f6fe1683d
Eseosa Otasowie <sosomartini@mail.fresnostate.edu>
2020-09-08 21:01:57 -0700

    rounding completed

--- Stamp_Program.py
+++ Stamp_Program.py
@@ -1,4 +1,3 @@
-
 """
 Start 
 get the number of sheets
@@ -7,17 +6,24 @@ round answer to next number
 output the result to the user
 end
 """
+import math 
+
+# input: sheet
 def calculate(sheet):
+    # step 1
     answer = sheet / 5
-    rounded =round(answer)
-    print("sheet is : ", answer)
+    # step 2
+    rounded = math.ceil(answer)
+    print("sheet is : ", sheet)
+    print("The answer is: ", answer)
     print("rounded is: ", rounded)
     Print("====================================")
+    # output: number of Stamps needed
     return rounded
 
-output = calculate (10000)
+output = calculate (16)
 
-print("the retutn statement is: ", ouput)
+print("The number of stamps needed is: ", ouput)
 
 
 
