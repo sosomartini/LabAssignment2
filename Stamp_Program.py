bd4e7f831fe061a25652419587d2fc810726633e
Eseosa Otasowie <sosomartini@mail.fresnostate.edu>
2020-09-08 20:33:32 -0700

    first commit

---
+++ Stamp_Program.py
@@ -0,0 +1,23 @@
+
+"""
+Start 
+get the number of sheets
+sheets / 5
+round answer to next number
+output the result to the user
+end
+"""
+def calculate(sheet):
+    answer = sheet / 5
+    rounded =round(answer)
+    print("sheet is : ", answer)
+    print("rounded is: ", rounded)
+    Print("====================================")
+    return rounded
+
+output = calculate (10000)
+
+print("the retutn statement is: ", ouput)
+
+
+
